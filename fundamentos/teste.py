9224f5113a62835b01d7387d6f413a0255edbf6e
Victor Dadério <155397909+victor071246@users.noreply.github.com>
2024-11-23 23:54:24 -0300

    Add files via upload

---
+++ fundamentos/teste.py
@@ -0,0 +1,23 @@
+# Primeiros Exemplos
+
+print('primeiro programa')
+
+1 + 2 + 3
+4 + 5 + 6
+
+print(1 + 2 + 3)
+print(4 + 5 + 6)
+
+help(print)
+
+# Tipos Básicos
+
+print(True)
+print(False)
+print(1.2 + 1)
+print(' Aqui eu falo minha língua!')
+print(" Também funciona")
+print(' Você é ' + 3 * 'muito ' + 'legal')
+# print(3 + 'a') -> ambiguidade
+print([1, 2, 3])
+print({'nome': 'pedro', 'idade': 22})
