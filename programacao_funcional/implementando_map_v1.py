94c81e7060e1f543f2793f2ee892717c397667a8
Victor Dadério <155397909+victor071246@users.noreply.github.com>
2024-12-05 22:52:41 -0300

    Add files via upload

---
+++ programacao_funcional/implementando_map_v1.py
@@ -0,0 +1,8 @@
+# Implementação simplificada do map
+def mapear(funcao, lista):
+    for elemento in lista:
+        yield funcao(elemento)
+
+if __name__ == "__main__":
+    resultado = (mapear(lambda x: x** 2, [2, 3, 4]))
+    print(list(resultado))
