f1419e91ac6e6ebcba8f7f74b56a6be097b19e08
Victor Dadério <155397909+victor071246@users.noreply.github.com>
2024-12-08 23:49:57 -0300

    Add files via upload

--- banco_de_dados_revisao/consultarComJoin.sql
+++ banco_de_dados_revisao/consultarComJoin.sql
@@ -1,1 +1,14 @@
-select * from estados
+select 
+e.nome as Estado,
+c.nome as Cidade, 
+e.regiao as 'Região'
+from estados e, cidades c
+where e.id = c.estado_id;
+
+select
+c.nome as Cidade,
+e.nome as Estado,
+regiao as 'Região'
+from estados e
+inner join cidades c
+on e.id = c.estado_id
